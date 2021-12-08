92577e9f68e97446baf5eabdfd75c5f693373de2
Dann-Go <dan6679990@gmail.com>
2021-12-08 17:20:42 +0300

    Adds Envs check. Makes Envs uppercase

--- internal/server.go
+++ internal/server.go
@@ -38,14 +38,45 @@ func initLogger() {
 	log.SetFormatter(&log.JSONFormatter{})
 }
 
+func envsCheck() {
+	_, present := os.LookupEnv("HOST")
+	if present == false {
+		log.Fatal("Environment isn't set")
+	}
+	_, present = os.LookupEnv("DBPORT")
+	if present == false {
+		log.Fatal("Environment isn't set")
+	}
+	_, present = os.LookupEnv("USERNAME")
+	if present == false {
+		log.Fatal("Environment isn't set")
+	}
+	_, present = os.LookupEnv("PASSWORD")
+	if present == false {
+		log.Fatal("Environment isn't set")
+	}
+	_, present = os.LookupEnv("DBNAME")
+	if present == false {
+		log.Fatal("Environment isn't set")
+	}
+	_, present = os.LookupEnv("SSLMODE")
+	if present == false {
+		log.Fatal("Environment isn't set")
+	}
+	_, present = os.LookupEnv("SERVPORT")
+	if present == false {
+		log.Fatal("Environment isn't set")
+	}
+}
+
 func Inject() *gin.Engine {
 	cfg := DbConfig{
-		Host:     os.Getenv("Host"),
-		Port:     os.Getenv("DBport"),
-		Username: os.Getenv("Username"),
-		Password: os.Getenv("Password"),
-		DBName:   os.Getenv("DBname"),
-		SSLMode:  os.Getenv("SSLmode"),
+		Host:     os.Getenv("HOST"),
+		Port:     os.Getenv("DBPORT"),
+		Username: os.Getenv("USERNAME"),
+		Password: os.Getenv("PASSWORD"),
+		DBName:   os.Getenv("DBNAME"),
+		SSLMode:  os.Getenv("SSLMODE"),
 	}
 	connection := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode)
@@ -70,6 +101,7 @@ func Inject() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 	router.Use(middleware.Logger())
+	router.Use(middleware.CORS())
 	bookRepo := postgres.NewPostgresqlRepository(db)
 	bookUsecase := usecase.NewBookUsecase(bookRepo, 30)
 	v := validator.New()
@@ -80,6 +112,7 @@ func Inject() *gin.Engine {
 }
 func (s *Server) Run(port string) error {
 	initLogger()
+	envsCheck()
 	router := Inject()
 	s.server = manners.NewWithServer(&http.Server{
 		Addr:           ":" + port,
