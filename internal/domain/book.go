a00057d77c556c9361c82f3d971aca381b901c21
Dann-Go <dan6679990@gmail.com>
2021-12-16 15:19:21 +0300

    Changes validation for bindings

--- internal/domain/book.go
+++ internal/domain/book.go
@@ -6,10 +6,10 @@ import (
 
 //Book ...
 type Book struct {
-	ID      int            `db:"id" json:"id" validate:"omitempty"`
-	Title   string         `db:"title" json:"title" validate:"required,gte=1"`
-	Authors pq.StringArray `db:"authors"  json:"authors" validate:"required,gte=1"`
-	Year    string         `db:"year" json:"year" validate:"required,datetime=2006-01-02"`
+	ID      int            `db:"id" json:"id" binding:"omitempty"`
+	Title   string         `db:"title" json:"title" binding:"required,gte=1"`
+	Authors pq.StringArray `db:"authors"  json:"authors" binding:"required,gte=1"`
+	Year    string         `db:"year" json:"year" binding:"required,datetime=2006-01-02"`
 }
 
 // BookUsecase represent the book's usecase
